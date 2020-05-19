7dc128c53fe3c25569a1a95e8273418d7bf6c2c5
jonasjs <joonasjss@gmail.com>
2020-05-18 21:13:52 -0300

    initial structure

---
+++ src/dev/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import QrCode from '../lib/index.svelte';  
+</script>
+
+
+<main class="container">
+  <QrCode />
+</main>
+
+<style>
+  
+</style>
---
+++ src/lib/index.svelte
@@ -0,0 +1,29 @@
+<script>
+  import QRious from 'qrious';
+
+  const QRcode = new QRious();
+
+
+  export let errorCorrection = "H";
+  export let background = "#fff";
+  export let color = "#000";
+  export let size = "400";
+  export let value = ""
+  export let padding = 0;
+
+  export let className = "qrcode"
+
+  QRcode.set({
+    background,
+    foreground: color,
+    level: errorCorrection,
+    padding,
+    size,
+    value,
+  });
+  
+  const image = QRcode.toDataURL('image/jpeg')
+
+</script>
+
+<img src={image} alt="" class={className} />
